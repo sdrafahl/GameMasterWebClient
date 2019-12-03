0adcf5e1b6bfc46b488091d222af26b32ea70913
Shane Drafahl <sdrafahl@pop-os.localdomain>
2019-12-02 22:47:11 -0600

    Getting basic page up which displays two buttons.

---
+++ dungeon-master-client/src/MainMenu/Menu.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import { Button } from '@material-ui/core';
+
+const MainMenu = () => {
+    return (
+        <div>
+            <Button variant="contained">Join a Game</Button>
+            <Button variant="contained">Start a Game</Button>
+        </div >
+    );
+};
+
+
+export default MainMenu;
